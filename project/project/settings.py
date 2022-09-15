1fc1d0a7197bbed449431f3ad5d8b3f742f37502
Tony <tonygr@live.ca>
2022-09-15 17:04:50 -0400

    update

--- project/project/settings.py
+++ project/project/settings.py
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
     'users.apps.UsersConfig',
-    'web.apps.WebConfig',
     'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
